e5af882bdc2841901a182adceeaefd64764b6561
Richard <rspence@softwareintitute.com>
2021-10-18 15:20:01 +0100

    Tests corrected

--- src/test/java/Fiction1Test.java
+++ src/test/java/Fiction1Test.java
@@ -1,18 +1,19 @@
 import com.softwareinstitute.training.richard.spence.Fiction1;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
-class FictionTest {
+public class Fiction1Test {
 
-    @org.junit.jupiter.api.Test
-    void getDate() {
+    @Test
+    public void getDate() {
         Fiction1 fiction = new Fiction1("Knausguard","2012");
         assertEquals("2012",fiction.getDate());
     }
 
-    @org.junit.jupiter.api.Test
-    void testGetDate() {
-        Fiction1 fiction = new Fiction1("Knausguard","2011");
+    @Test
+    public void testGetDate() {
+        Fiction1 fiction = new Fiction1("Knausguard","This is the Right one");
         assertEquals("This is the Right one","This is the Right one",fiction.getDate());
     }
 }
